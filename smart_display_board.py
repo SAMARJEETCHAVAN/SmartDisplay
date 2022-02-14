7d48548f05e6530eb81b304bf3f3596a39d14c2d
samarjeet.chavan <samarjeet.chavan92@gmail.com>
2022-02-14 12:48:39 +0530

    Removed comments and updated existing links

--- smart_display_board.py
+++ smart_display_board.py
@@ -5,24 +5,14 @@ import os
 from time import sleep
 #import gspread
 sleep(30)
-"""screen_size=0
-os.system('pkill -o chromium')
-os.system("pkill feh")
-try:
-    os.system('python /home/pi/find_screen_size.py')
-except:
-    temp = 'temp'
-f = open("Screen_size.txt", "r")
-screen_size=(f.read())
-screen_width = screen_size[:screen_size.index(' ')]
-screen_height = screen_size[screen_size.index(' ')+1:]"""
-time1 = 30
+time1 = 5
 time2 = 120
 time3 = 300
 
 list_of_links = ['http://192.168.2.11/smartview/punnet_view.php',
                  'http://192.168.2.11/smartview/inwt_view.php',
                  'http://192.168.2.11/smartview/knitterband_view.php',
+                 'http://192.168.2.11/smartview/box_view.php',
                  'http://192.168.2.11/smartview/9DaysTop3Packing.php',
                  'http://192.168.2.11/smartview/9DaysTop3sorting.php',
                  'http://192.168.2.11/smartview/ReportPackersPerformance.php',
@@ -36,9 +26,9 @@ while True:
     try:
         for link in list_of_links:
             os.system('chromium-browser --start-fullscreen  '+link+'&')
-            sleep(120)
+            sleep(time2)
             os.system('pkill -o chromium')
-            sleep(1)
+            sleep(time1)
     except:
         tryagain='tryagain'
         print('There was some Error')
